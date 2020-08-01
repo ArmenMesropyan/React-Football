ddee28010f32fb0f8ecedff48260a93ddd1ba2f1
ArmenMesropyan <mesropyan200315@gmail.com>
2020-08-01 17:58:34 +0400

    Header stylization and grid added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,13 +4,15 @@ const Header = () => (
     <header className="home-page__header first">
         <div className="container">
             <ul className="first__elements row">
-                <li className="first__element">
-                    <h2 className="first__title">Test Title</h2>
-                    <p className="first__text">Test text</p>
+                <li className="first__element first__element--info col-md-6">
+                    <h2 className="first__title">Title</h2>
+                    <p className="first__text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laudantium, iure optio aperiam, excepturi animi nihil id blanditiis doloribus ratione repellendus impedit similique. Natus et rerum minus magni? Laudantium, voluptate.
+                    </p>
                     <button type="button" className="first__btn">Click me</button>
                 </li>
-                <li className="first__element first__element--image">
-                    <img src="/images/header-image.png" alt="Eden Hazard" />
+                <li className="first__element first__element--image col-md-6">
+                    <img src="/images/header-image.png" alt="Eden Hazard" className="first__img"/>
                 </li>
             </ul>
         </div>
