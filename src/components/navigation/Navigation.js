985ba0697aa02eba79ace003cdb3e53d96b482e8
ArmenMesropyan <mesropyan200315@gmail.com>
2020-08-01 15:19:32 +0400

    Navigation links stylization with hover transition, main nav dashed line added

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -4,11 +4,15 @@ const Navigation = () => (
     <nav className="home-page__navigation main-nav">
         <div className="container">
             <ul className="main-nav__list">
-                <li className="main-nav__item logo">
+                <li className="main-nav__item main-nav__item--logo logo">
                     <strong className="logo__text">
                         <span className="logo__text logo__text--small">Football</span>
                         <span className="logo__text logo__text--big">X</span>
                     </strong>
+                </li>
+                <li className="main-nav__item main-nav__links">
+                    <a href="#" className="main-nav__link">Home</a>
+                    <a href="#" className="main-nav__link">Leagues</a>
                 </li>
             </ul>
         </div>
